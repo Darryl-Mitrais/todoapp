b4e431e7288d7716713d5d2e74379d7f2d90c93d
Darryl-Mitrais <cristoforus_d909@mitrais.com>
2021-07-19 14:17:06 +0700

    Add router to web app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import ListToDo from './components/ListToDo/ListToDo';
+import Login from './components/Login/Login';
+import CreateToDo from './components/CreateToDo/CreateToDo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Router>
+        <h2>To Do App</h2>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={Login}></Route>
+            <Route path="/tasks" component={ListToDo}></Route>
+            <Route path="/add-task" component={CreateToDo}></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
